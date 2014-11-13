718d9816eef50c089007a67c7d0854d8b312df4c
Thomas Nabord <thomas.nabord@prestashop.com>
2014-11-13 10:55:08 +0100

    Version number fix

--- shoppingfluxexport.php
+++ shoppingfluxexport.php
@@ -36,7 +36,7 @@ class ShoppingFluxExport extends Module
 	{
 		$this->name = 'shoppingfluxexport';
 		$this->tab = 'smart_shopping';
-		$this->version = '3.9.8.1';
+		$this->version = '3.9.9';
 		$this->author = 'PrestaShop';
 		$this->limited_countries = array('fr', 'us');
 
