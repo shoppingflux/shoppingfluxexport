187fbf227894d4f9a3aa26307a5713686045e4fa
clement-hollander <clement@shopping-feed.com>
2017-02-22 11:58:01 +0100

    Update shoppingfluxexport.php

--- shoppingfluxexport.php
+++ shoppingfluxexport.php
@@ -42,7 +42,7 @@ class ShoppingFluxExport extends Module
     {
         $this->name = 'shoppingfluxexport';
         $this->tab = 'smart_shopping';
-        $this->version = '4.2.1';
+        $this->version = '4.2.3';
         $this->author = 'PrestaShop';
         $this->limited_countries = array('fr', 'us');
         $this->module_key = '08b3cf6b1a86256e876b485ff9bd4135';
