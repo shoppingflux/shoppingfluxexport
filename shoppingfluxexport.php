a199bce9fc61c2604d8fb58ccb5012af8b864dd7
vincent-coll <vincent.coll@profileo.com>
2018-01-29 14:30:51 +0100

    Correct allready by already

--- shoppingfluxexport.php
+++ shoppingfluxexport.php
@@ -1569,17 +1569,17 @@ class ShoppingFluxExport extends Module
                                 $order->ShippingMethod = 'Mondial Relay';
                             }
         
-                            // Check if the order allready exists by lookig at the messages in the order
+                            // Check if the order already exists by lookig at the messages in the order
                             $orderExists = Db::getInstance()->getRow('SELECT m.id_message, m.id_order FROM '._DB_PREFIX_.'message m
                                 WHERE m.message LIKE "%Numéro de commande '.pSQL($order->Marketplace).' :'.pSQL($order->IdOrder).'%"');
         
                             if (! $forcedOrder && isset($orderExists['id_message']) && isset($orderExists['id_order'])) {
-                                SfLogger::getInstance()->log(SF_LOG_ORDERS, 'Order allready exists (id = '.$order->IdOrder.'): notifying ShoppingFlux', $doEchoLog);
+                                SfLogger::getInstance()->log(SF_LOG_ORDERS, 'Order already exists (id = '.$order->IdOrder.'): notifying ShoppingFlux', $doEchoLog);
                                 $this->_validOrders((string)$order->IdOrder, (string)$order->Marketplace, (int)$orderExists['id_order']);
                                 continue;
                             }
                             
-                            // Check if the order allready exists by lookig at the adress alias and first/last name
+                            // Check if the order already exists by lookig at the adress alias and first/last name
                             $orderExists = Db::getInstance()->getRow("SELECT * FROM " . _DB_PREFIX_ . "orders o, " . _DB_PREFIX_ . "customer c, " . _DB_PREFIX_ . "address a
                                 WHERE o.id_customer = c.id_customer
                                 AND c.email = 'do-not-send@alerts-shopping-flux.com'
@@ -1591,7 +1591,7 @@ class ShoppingFluxExport extends Module
                             
                             if (! $forcedOrder && isset($orderExists['id_order']) && isset($orderExists['id_order'])) {
                                 // This is the second try of an order creation, last process could not be completed
-                                SfLogger::getInstance()->log(SF_LOG_ORDERS, 'Order allready exists (id = ' . $order->IdOrder . '): notifying ShoppingFlux', $doEchoLog);
+                                SfLogger::getInstance()->log(SF_LOG_ORDERS, 'Order already exists (id = ' . $order->IdOrder . '): notifying ShoppingFlux', $doEchoLog);
                             
                                 // Re set the carrier
                                 $orderLoaded = new Order((int) $orderExists['id_order']);
@@ -3105,7 +3105,7 @@ class ShoppingFluxExport extends Module
     {
         $lastOrderstreated = $this->getLastOrdersTreated();
 
-        // Check if allready existing
+        // Check if already existing
         if (! in_array((string)$order->IdOrder, array_keys($lastOrderstreated))) {
             if (sizeof($lastOrderstreated) > 19) {
                 $lastOrderstreated = array_slice($lastOrderstreated, 0, 19);
