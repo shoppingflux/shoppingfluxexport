add9f827177e36cd38ee28cefebbd8acb5c8da73
Maxime Morel-Bailly <17177824+infiniweb@users.noreply.github.com>
2018-03-09 17:21:36 +0400

    network-path reference for jquery

--- debug.php
+++ debug.php
@@ -451,7 +451,7 @@ function logDebug($toLog)
 ?>
 <html>
 <head>
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.7.2.js"></script>
 
 <style type="text/css">
 label {
